ffec6d87dbf973701341b5e5ed9278a7fa656391
Chen Yang <yangc5@mail.uc.edu>
2016-06-16 16:13:07 -0500

    added bot choice in html

---
+++ script.js
@@ -0,0 +1,7 @@
+'use strict';
+
+$(document).ready(function(){
+  console.log('ready');
+  var signWrapperWidth = $('.sign-wrapper').width();
+  $('.sign-wrapper').css({'height':signWrapperWidth+'px'});
+})
